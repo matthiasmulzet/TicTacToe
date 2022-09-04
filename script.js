0c29fcbaaae949cd62996b3de7aab10c683b0d5e
matthiasmulzet <mulzetmatthias@gmail.com>
2022-09-04 14:25:51 +0200

    changed code

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-let numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+let fields = [];
 
-let showCross = false;
-let showCircle = false;
+let currentShape = 'cross';
 
-function showCircleOrCross(circleId, crossId) {
-    let circleIdNumber = circleId.slice(-1);
-    let crossIdNumber = crossId.slice(-1);
 
-    let actualCross = document.getElementById(`cross-${crossIdNumber}`);
-    let actualCircle = document.getElementById(`circle-${circleIdNumber}`);
-    let actualCrossIdTd = document.getElementById(`circle-or-cross-id-${crossIdNumber}`);
-    let actualCircleIdTd = document.getElementById(`circle-or-cross-id-${circleIdNumber}`);
-    if(showCross == false) {
-        if (typeof actualCrossIdTd.onclick == "function") {
-            actualCross.classList.remove('d-none');
-            actualCrossIdTd.removeAttribute('onclick');
-            actualCircleIdTd.removeAttribute('onclick');
-            showCross = true;
-        } 
+function fillShape(id) {
+    if (currentShape == 'cross') {
+        currentShape = 'circle';
+    } else {
+        currentShape = 'cross';
     }
+    fields[id] = currentShape;
+    console.log(fields);
+    draw();
+}
 
-    else {
-        if (typeof actualCircleIdTd.onclick == "function") {
-            actualCircle.classList.remove('d-none');
-            actualCrossIdTd.removeAttribute('onclick');
-            actualCircleIdTd.removeAttribute('onclick');
-            showCross = false;
-        } 
+
+function draw() {
+    for(let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'circle') {
+            document.getElementById('circle-' + i).classList.remove('d-none');
+        }
+
+        if (fields[i] == 'cross') {
+            document.getElementById('cross-' + i).classList.remove('d-none');
+        }
     }
 }
