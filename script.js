fb1862917bb48a35eb880d928c9b1f7719730dc2
matthiasmulzet <mulzetmatthias@gmail.com>
2022-09-08 21:32:29 +0200

    finished html and css

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let fields = [];
+
 let gameOver = false;
 let draw = false;
+
+let playerStart = 'cross';
 let currentShape = 'cross';
+
 let circlePoints = 0;
 let crossPoints = 0;
+
 let AUDIO_CLICK = new Audio('audio/click.mp3');
 let AUDIO_WINNER = new Audio('audio/winner.mp3');
 
@@ -11,23 +16,36 @@ let AUDIO_WINNER = new Audio('audio/winner.mp3');
 function fillShape(id) {
     if (!fields[id] && !gameOver) {
         AUDIO_CLICK.play();
-        if (currentShape == 'cross') {
-            currentShape = 'circle';
-            document.getElementById('player-2').classList.remove('player-inactive');
-            document.getElementById('player-1').classList.add('player-inactive');
-        } 
-        
-        else {
-            currentShape = 'cross';
-            document.getElementById('player-1').classList.remove('player-inactive');
-            document.getElementById('player-2').classList.add('player-inactive');
-        }
-        
+        whichPlayerTurn();
         fields[id] = currentShape;
-        console.log(fields);
         updateField();
         checkForWin();
     }
+}
+
+
+function whichPlayerTurn() {
+    if (currentShape == 'cross') {
+        showPlayerOne();
+    }
+
+    else {
+        showPlayerTwo();
+    }
+}
+
+
+function showPlayerOne() {
+    currentShape = 'circle';
+    document.getElementById('player-2').classList.remove('player-inactive');
+    document.getElementById('player-1').classList.add('player-inactive');
+}
+
+
+function showPlayerTwo() {
+    currentShape = 'cross';
+    document.getElementById('player-1').classList.remove('player-inactive');
+    document.getElementById('player-2').classList.add('player-inactive');
 }
 
 
@@ -46,7 +64,28 @@ function updateField() {
 
 function restart() {
     gameOver = false;
+    removeSymbolsAndLines();
+    document.getElementById('winner-container').classList.add('d-none');
+    document.getElementById("draw-container").classList.add('d-none');
+    fields = [];
+    whichPlayerStart();
+}
 
+
+function whichPlayerStart() {
+    if (playerStart == 'cross') {
+        playerStart = 'circle';
+        showPlayerOne();
+    }
+
+    else {
+        playerStart = 'cross';
+        showPlayerTwo();
+    }
+}
+
+
+function removeSymbolsAndLines() {
     for (let i = 1; i < 9; i++) {
         document.getElementById('line-' + i).style.transform = '';
     }
@@ -55,130 +94,65 @@ function restart() {
         document.getElementById('circle-' + i).classList.add('d-none');
         document.getElementById('cross-' + i).classList.add('d-none');
     }
-
-    document.getElementById('winner-container').classList.add('d-none');
-    document.getElementById("draw-container").classList.add('d-none');
-    fields = [];
-    currentShape = "cross";
-    document.getElementById('player-1').classList.remove('player-inactive');
-    document.getElementById('player-2').classList.add('player-inactive');
 }
 
 
 function checkForWin() {
     let winner;
 
-    // First Row
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
-        if (fields[0] == 'cross') {
-            document.getElementById('winner-image').src = "./img/cross.png";
-            crossPoints++;
-        }
-
-        if (fields[0] == 'circle') {
-            document.getElementById('winner-image').src = "./img/circle.png";
-            circlePoints++;
-        }
+        winnerCircleOrCross(0);
 
         document.getElementById('line-1').style.transform = 'scaleX(1)';
     }
 
     else if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
-        if (fields[3] == 'cross') {
-            document.getElementById('winner-image').src = "./img/cross.png";
-            crossPoints++;
-        }
+        winnerCircleOrCross(3);
 
-        if (fields[3] == 'circle') {
-            document.getElementById('winner-image').src = "./img/circle.png";
-            circlePoints++;
-        }
         document.getElementById('line-2').style.transform = 'scaleX(1)';
     }
 
     else if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
-        if (fields[6] == 'cross') {
-            document.getElementById('winner-image').src = "./img/cross.png";
-            crossPoints++;
-        }
+        winnerCircleOrCross(6);
 
-        if (fields[6] == 'circle') {
-            document.getElementById('winner-image').src = "./img/circle.png";
-            circlePoints++;
-        }
         document.getElementById('line-3').style.transform = 'scaleX(1)';
     }
 
     else if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
-        if (fields[0] == 'cross') {
-            document.getElementById('winner-image').src = "./img/cross.png";
-            crossPoints++;
-        }
+        winnerCircleOrCross(0);
 
-        if (fields[0] == 'circle') {
-            document.getElementById('winner-image').src = "./img/circle.png";
-            circlePoints++;
-        }
         document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)';
     }
 
     else if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
-        if (fields[1] == 'cross') {
-            document.getElementById('winner-image').src = "./img/cross.png";
-            crossPoints++;
-        }
+        winnerCircleOrCross(1);
 
-        if (fields[1] == 'circle') {
-            document.getElementById('winner-image').src = "./img/circle.png";
-            circlePoints++;
-        }
         document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)';
     }
 
     else if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
-        if (fields[2] == 'cross') {
-            document.getElementById('winner-image').src = "./img/cross.png";
-            crossPoints++;
-        }
+        winnerCircleOrCross(2);
 
-        if (fields[2] == 'circle') {
-            document.getElementById('winner-image').src = "./img/circle.png";
-            circlePoints++;
-        }
         document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1)';
     }
 
     else if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
-        if (fields[0] == 'cross') {
-            document.getElementById('winner-image').src = "./img/cross.png";
-            crossPoints++;
-        }
+        winnerCircleOrCross(0);
 
-        if (fields[0] == 'circle') {
-            document.getElementById('winner-image').src = "./img/circle.png";
-            circlePoints++;
-        }
         document.getElementById('line-7').style.transform = 'rotate(45deg) scaleX(1)';
     }
 
     else if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
-        if (fields[2] == 'cross') {
-            document.getElementById('winner-image').src = "./img/cross.png";
-            crossPoints++;
-        }
+        winnerCircleOrCross(2);
 
-        if (fields[2] =='circle') {
-            document.getElementById('winner-image').src = "./img/circle.png";
-            circlePoints++;
-        }
         document.getElementById('line-8').style.transform = 'rotate(-45deg) scaleX(1)';
     }
 
@@ -194,8 +168,30 @@ function checkForWin() {
 
     if (fields[0] && fields[1] && fields[2] && fields[3] && fields[4]
         && fields[5] && fields[6] && fields[7] && fields[8] && gameOver == false) {
-            document.getElementById("draw-container").classList.remove('d-none');
-        }
+        document.getElementById("draw-container").classList.remove('d-none');
+    }
+}
 
+
+function winnerCircleOrCross(fieldNumber) {
+    if (fields[fieldNumber] == 'cross') {
+        checkWinnerCross();
+    }
+
+    if (fields[fieldNumber] == 'circle') {
+        checkWinnerCircle();
+    }
+}
+
+
+function checkWinnerCross() {
+    document.getElementById('winner-image').src = "./img/cross.png";
+    crossPoints++;
+}
+
+
+function checkWinnerCircle() {
+    document.getElementById('winner-image').src = "./img/circle.png";
+    circlePoints++;
 }
 
